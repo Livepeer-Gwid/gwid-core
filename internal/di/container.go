25a5a081934771790fae68e50ee6814a069e1ba3
Charles Duruaku <charlesrichson@gmail.com>
2025-06-24 15:06:51 +0100

    chore/postgres-setup

--- internal/di/container.go
+++ internal/di/container.go
@@ -1,9 +1,22 @@
+// Package di mounts all services
 package di
 
-import "gwid.io/gwid-core/internal/config"
+import (
+	"gorm.io/gorm"
+	"gwid.io/gwid-core/internal/config"
+	"gwid.io/gwid-core/internal/database"
+)
 
-type Container struct{}
+type Container struct {
+	DB *gorm.DB
+}
 
 func NewContainer(conf *config.Config) *Container {
-	return &Container{}
+	database.InitDB(conf)
+
+	db := database.DB
+
+	return &Container{
+		DB: db,
+	}
 }
