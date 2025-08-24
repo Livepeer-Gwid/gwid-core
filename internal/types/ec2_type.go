0c268d151bb73cbd00a1c60e92c63a8b9e153460
Charles Duruaku <charlesrichson@gmail.com>
2025-08-24 14:35:22 +0100

    feat: ssm command implementation

--- internal/types/ec2_type.go
+++ internal/types/ec2_type.go
@@ -10,3 +10,9 @@ type CommandResult struct {
 	StandardErr   string
 	ExecutionTime time.Duration
 }
+
+const EC2UserData = `#!/bin/bash
+apt-get update -y
+snap install amazon-ssm-agent --classic
+systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
+systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service`
